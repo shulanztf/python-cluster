83c74d80491c9f237d9c6676eb0a8dc14ea46f63
1426763507 <1426763507@qq.com>
2017-08-07 10:28:20 +0800

    python 学习 初次提交

---
+++ com/hhcf/crawler/__init__.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@version: 1.0
+@author: zhaotf
+@file: __init__.py.py
+@time: 2017/8/7 0007 10:22
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
