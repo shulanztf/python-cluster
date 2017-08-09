1dbea0b7ab28048e3b64869dce45e28f657ba65b
1426763507 <1426763507@qq.com>
2017-08-09 11:29:49 +0800

    python 函数相关

--- com/hhcf/crawler/crawler-1.py
+++ com/hhcf/crawler/crawler-1.py
@@ -8,6 +8,28 @@
 """
 
 
+# 网页 http://blog.csdn.net/fly_yr/article/details/51525435
+import urllib.request
+
+# 网址
+url = "http://www.douban.com/"
+# 请求
+request = urllib.request.Request(url)
+# 爬取结果
+response = urllib.request.urlopen(request)
+data = response.read()
+# 设置解码方式
+data = data.decode("utf-8")
+# 打印结果
+print(data)
+# 打印爬取网页信息
+print(type(response))
+print(response.geturl())
+print(response.info())
+print(response.getcode())
+
+
+
 def func():
     pass
 
