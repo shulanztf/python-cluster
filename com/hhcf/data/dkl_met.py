34d4513b70722f7e0c9b920e7f183e1a2360ddb8
1426763507 <1426763507@qq.com>
2017-08-11 16:32:39 +0800

    python资料补充

---
+++ com/hhcf/data/dkl_met.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@version: 1.0
+@author: zhaotf
+@file: dkl_met.py
+@time: 2017/8/11 0011 14:44
+"""
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from matplotlib.ticker import FuncFormatter
+
+df = pd.read_excel("https://github.com/chris1610/pbpython/blob/master/data/sample-salesv3.xlsx?raw=true", 'r')
+
+df.head()
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
